560398cb11ac9e43da9e8189bd8c49f2008ea9f4
Zachary Fonseca <zachary.fonseca17@my.stjohns.edu>
2019-09-11 16:50:25 -0400

    Lab 1

---
+++ cus1166_lab1/playground.py
@@ -0,0 +1,6 @@
+print("Hello Word")   # Display a message
+# Get user input and display a message.
+myname = input("What is your name: ")
+print("Hello " + str(myname))
+# Alternative way to format a string
+print("Hello %s" % myname)
